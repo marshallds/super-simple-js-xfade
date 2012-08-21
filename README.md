f879009c9955611a47207a6a83abe6741153d34e
Marshallds <marshall.ds@gmail.com>
2012-08-21 13:49:07 -0600

    added example file. abstracted settings in the js.

--- README.md
+++ README.md
@@ -2,3 +2,7 @@ super-simple-js-xfade
 =====================
 
 Really simple way to cross fade through a bunch of divs
+
+
+
+This script will crossfade the divs in a container element with id="banner".  The styles in the example file are required for it to work properly.
--- the.js
+++ the.js
@@ -1,3 +1,7 @@
+// Requires jQuery
+
+	var fadeDuration = 1000;
+	var fadeInterval = 5000;
 
 	$(document).ready(function() {
 		var bannerRotate = function() {
@@ -13,10 +17,10 @@
 			var c=0;
 
 			setInterval(function() {
-				$('div[sequence='+c+']').fadeOut(1000);
+				$('div[sequence='+c+']').fadeOut(fadeDuration);
 				(c<frameCount) ? c++ : c=0;
-				$('div[sequence='+c+']').fadeIn(1000);
-			}, 5000);
+				$('div[sequence='+c+']').fadeIn(fadeDuration);
+			}, fadeInterval);
 		};
 
 		bannerRotate();
